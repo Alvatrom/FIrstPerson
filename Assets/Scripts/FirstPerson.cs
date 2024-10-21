d50037170433c0d8c1d42df98887d112f7b75b87
Alvatrom <alvarogomez0402@gmail.com>
2024-10-21 20:53:37 +0200

    TERCERA PERSONA

--- Assets/Scripts/FirstPerson.cs
+++ Assets/Scripts/FirstPerson.cs
@@ -18,23 +18,29 @@ public class FirstPerson : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        MoverRotar();
-
-
+        MoverYRotar();
     }
-    void MoverRotar()
+
+    private void MoverYRotar()
     {
         float h = Input.GetAxisRaw("Horizontal");
         float v = Input.GetAxisRaw("Vertical");
-        Vector3 movimiento = new Vector3(h,0,v).normalized;
 
-        float anguloRotacion = Mathf.Atan2(movimiento.x, movimiento.z) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
-        if (movimiento.magnitude > 0)
+        Vector2 input = new Vector2(h, v).normalized;
+
+        //calculo el angulo al que tengo que rotarme en funcion de los inputs y camara
+        float angulo = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+
+        //el cuerpo quede orientado hacia donde me muevo(camara)
+        transform.eulerAngles = new Vector3(0, angulo, 0);
+
+        //si el jugador ha tocado teclas...
+        if (input.magnitude > 0)
         {
-            //el cuerpo quede orientado hacia donde me muevo(camara)
-            transform.eulerAngles = new Vector3(0, anguloRotacion, 0);
-            controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+            //Mi movimiento tambien ha quedado rotado en base al angulo calculado
+            Vector3 movimiento = Quaternion.Euler(0, angulo, 0) * Vector3.forward;
+
+            controller.Move(input * velocidadMovimiento * Time.deltaTime);
         }
-        
     }
 }
---
+++ Assets/Scripts/ThirdPerson.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPerson : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private float smoothTime;
+
+    CharacterController controller;
+
+    private float velocidadRotacion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoverYRotar();
+    }
+
+    private void MoverYRotar()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        Vector2 input = new Vector2(h, v).normalized;
+
+
+        //si el jugador ha tocado teclas...
+        if (input.sqrMagnitude > 0)
+        {
+            //calculo el angulo al que tengo que rotarme en funcion de los inputs y camara
+            float angulo = Mathf.Atan2(input.x, input.y) * Mathf.Rad2Deg + Camera.main.transform.eulerAngles.y;
+
+            float anguloSuave = Mathf.SmoothDampAngle(transform.eulerAngles.y, angulo,ref velocidadRotacion, smoothTime);
+
+
+            //el cuerpo quede orientado hacia donde me muevo(camara)
+            transform.eulerAngles = new Vector3(0, anguloSuave, 0);
+            //Mi movimiento tambien ha quedado rotado en base al angulo calculado
+            Vector3 movimiento = Quaternion.Euler(0, angulo, 0) * Vector3.forward;
+
+            controller.Move(movimiento * velocidadMovimiento * Time.deltaTime);
+        }
+    }
+}
