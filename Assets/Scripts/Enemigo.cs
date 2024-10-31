731c50230b16fa392eb121db9898af9f943911b4
Alvatrom <alvarogomez0402@gmail.com>
2024-10-31 19:28:42 +0100

    Zombie
    probabilidad de animacion, ajustes de distancias zombie, distintos tipos de navemesh

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -7,13 +7,25 @@ public class Enemigo : MonoBehaviour
 {
     private NavMeshAgent agent;
     [SerializeField] private FirstPerson player;
-
+    Animator animator;
+    int correrAnim;
     //el enemigo tiene que perseguir al player
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         player = FirstPerson.FindObjectOfType<FirstPerson>();
+        animator = GetComponent<Animator>();
+
+        correrAnim = Random.Range(0,2);
+        if (correrAnim == 0)
+        {
+            animator.SetBool("RunNormal", true);
+        }
+        else
+        {
+            animator.SetBool("RunHerido", true);
+        }
     }
 
     // Update is called once per frame
