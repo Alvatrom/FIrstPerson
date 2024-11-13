7f3786436e5713a51fe95390c53376235b6024e3
Alvatrom <alvarogomez0402@gmail.com>
2024-11-13 17:28:45 +0100

    caja, sistema de interacion

---
+++ Assets/Scripts/Arma/AmmoBox.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour
+{
+    private Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void AbrirCaja()
+    {
+        anim.SetTrigger("abrir");
+    }
+    public void CerrarCaja()
+    {
+        anim.SetTrigger("cerrar");
+    }
+}
---
+++ Assets/Scripts/SIstemaDeInteraciones.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.VirtualTexturing;
+
+public class SIstemaDeInteraciones : MonoBehaviour
+{
+    private Camera cam;
+    private Transform interactuableActual;
+    [SerializeField] private float distanciaInterracion;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+    private void Update()
+    {
+        DeteccionInteractuable();
+    }
+
+    private void DeteccionInteractuable()
+    {
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, distanciaInterracion))
+        {
+            if (hit.transform.TryGetComponent(out AmmoBox scriptAmmobox))
+            {
+                interactuableActual = scriptAmmobox.transform;
+                interactuableActual.GetComponent<Outline>().enabled = true;
+                if (Input.GetKey(KeyCode.E))
+                {
+                    scriptAmmobox.AbrirCaja();
+                }
+            }
+        }
+        else if (interactuableActual != null)
+        {
+            interactuableActual.GetComponent<Outline>().enabled = false;
+            interactuableActual = null;
+        }
+    }
+
+    
+}
